6635507fe7a8badc058c375eff0e01aa5b58415c
VuThanhSang <zimb240@gmail.com>
2022-11-29 14:47:29 +0700

    .

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,41 +1,53 @@
-const URL = 'http://localhost:3240/';
+const API_URL = 'http://localhost:8000';
 
+// Load planets and return as JSON.
 async function httpGetPlanets() {
-    // TODO: Once API is ready.
-    // Load planets and return as JSON.
-    const res = await fetch(`${URL}planets`);
-    return await res.json();
+  const response = await fetch(`${API_URL}/planets`);
+  return await response.json();
 }
 
+  // Load launches, sort by flight number, and return as JSON.
 async function httpGetLaunches() {
-    // TODO: Once API is ready.
-    // Load launches, sort by flight number, and return as JSON.
-    const res = await fetch(`${URL}launches`);
-    const fetchedLaunches = await res.json();
-    return await fetchedLaunches.sort((a, b) => {
-        return a.flightNumber - b.flightNumber;
-    });
+  const response = await fetch(`${API_URL}/launches`);
+  const fetchedLaunches = await response.json();
+  return fetchedLaunches.sort((a, b) => {
+    return a.flightNumber - b.flightNumber;
+  });
 }
-
+  // Submit given launch data to launch system.
 async function httpSubmitLaunch(launch) {
-    // TODO: Once API is ready.
-    // Submit given launch data to launch system.
-    try {
-        return await fetch(`${URL}launches`, {
-            method: 'post',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(launch),
-        });
-    } catch (error) {
-        return { ok: false };
-    }
+  try {
+    return await fetch(`${API_URL}/launches`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(launch),
+    });
+  } catch(err) {
+    return {
+      ok: false,
+    };
+  }
 }
 
+  // Delete launch with given ID.
 async function httpAbortLaunch(id) {
-    // TODO: Once API is ready.
-    // Delete launch with given ID.
+  try {
+    return await fetch(`${API_URL}/launches/${id}`, {
+      method: "delete",
+    });
+  } catch(err) {
+    console.log(err);
+    return {
+      ok: false,
+    };
+  }
 }
 
-export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch };
+export {
+  httpGetPlanets,
+  httpGetLaunches,
+  httpSubmitLaunch,
+  httpAbortLaunch,
+};
